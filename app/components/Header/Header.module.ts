541e8c0cb13e64ff7ce19658f6e98715d7a3771b
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-12 11:24:47 -0300

    Criando estrutura inicial do header para mobile

---
+++ app/components/Header/Header.module.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  height: 50px;
+
+  border-bottom: 1px solid;
+  border-color: #27272a;
+  border-bottom-width: 1px;
+
+  padding-left: 5%;
+  padding-right: 5%;
+
+  margin-bottom: 5rem;
+
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.tablet}px) {
+    background-color: #27272a;
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoints.tablet}px) {
+    background-color: yellow;
+  }
+
+  @media (max-width: ${(props) => props.theme.breakpoints.mobile}px) {
+    background-color: green;
+  }
+`;
+
+interface SideMenuProps {
+  isopen: boolean;
+}
+
+export const SideMenu = styled.div<SideMenuProps>`
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #18181b;
+  color: #fff;
+  transform: ${(props) =>
+    props.isopen ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.4s ease-in-out;
+`;
+
+export const SideMenuContent = styled.div`
+  padding-top: 50px;
+`;
+
+export const CloseContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top: -8px;
+  right: 3px;
+  margin: 15px;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: #27272b66;
+  padding: 8px;
+`;
+
+export const MenuItemContainer = styled.div`
+  padding: 24px;
+
+  border-bottom: 1px solid #27272a;
+`;
+
+export const ItemTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+
+  font-size: 18px;
+`;
+
+export const ContentContainer = styled.div`
+  height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavItems = styled.nav`
+  display: none;
+`;
+
+export const NavContainer = styled.ul`
+  display: flex;
+  column-gap: 2rem;
+`;
+
+export const NavTitle = styled.li`
+  color: #fff;
+`;
---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,90 @@
+'use client';
+import { FaBars, FaUserTie, FaLaptopCode } from 'react-icons/fa6';
+import { IoCloseOutline } from 'react-icons/io5';
+import { AiOutlineRead } from 'react-icons/ai';
+import {
+  CloseContainer,
+  ContentContainer,
+  HeaderContainer,
+  ItemTitle,
+  MenuItemContainer,
+  NavContainer,
+  NavItems,
+  NavTitle,
+  SideMenu,
+  SideMenuContent,
+} from './Header.module';
+import { useState } from 'react';
+
+interface IMenuItem {
+  id: number;
+  title: string;
+  icon: React.FC<{ size?: number; color?: string }>;
+}
+
+const menuItens: IMenuItem[] = [
+  {
+    id: 1,
+    title: 'About',
+    icon: FaUserTie,
+  },
+  {
+    id: 2,
+    title: 'Projects',
+    icon: FaLaptopCode,
+  },
+  {
+    id: 3,
+    title: 'Blog',
+    icon: AiOutlineRead,
+  },
+];
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <HeaderContainer>
+      {/* Menu deslizante */}
+      <SideMenu isopen={isOpen || false}>
+        <CloseContainer onClick={() => setIsOpen(false)}>
+          <IoCloseOutline size={25} />
+        </CloseContainer>
+        <SideMenuContent>
+          {menuItens.map((item) => (
+            <MenuItemContainer key={item.id}>
+              <ItemTitle>
+                <item.icon size={18} color={'#71717a'} />
+                <p>{item.title}</p>
+              </ItemTitle>
+            </MenuItemContainer>
+          ))}
+        </SideMenuContent>
+      </SideMenu>
+      <ContentContainer>
+        <p style={{ color: '#fff' }}>Image</p>
+        <NavItems>
+          <NavContainer>
+            <NavTitle>About</NavTitle>
+            <NavTitle>Projects</NavTitle>
+            <NavTitle>Blog</NavTitle>
+          </NavContainer>
+        </NavItems>
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          style={{
+            color: '#fff',
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: '25%',
+            backgroundColor: '#27272b66',
+            padding: '8px',
+          }}
+        >
+          <FaBars size={16} />
+        </div>
+      </ContentContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
