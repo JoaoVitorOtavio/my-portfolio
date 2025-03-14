c3638133e5cba0cc41932c9af5875bc1fd127223
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-13 21:59:40 -0300

    Adicionando data para works

---
+++ app/data/works.ts
@@ -0,0 +1,37 @@
+import { IWorkExperience } from '../components/WorkExperience/interfaces';
+
+export const Works: IWorkExperience[] = [
+  {
+    enterpriseName: 'Sonoma Market',
+    description:
+      'Desenvolvi e mantive sistemas usando ReactJS, Node.js e NestJS, incluindo um backoffice integrado ao ERP Millennium, um e-commerce completo e uma solução para transferência de produtos entre marketplaces. Também criei um Design System com Storybook e atuei em um projeto freelance de gestão de vendas.',
+    imagePath: '/sonoma_logo.jpg',
+    role: 'Full Stack Developer',
+    time: 'Jun, 2021 - Fev, 2025',
+  },
+  {
+    enterpriseName: 'VIAMAKER® Education',
+    description:
+      'Responsável pelo desenvolvimento do front end de uma plataforma com o objetivo de planejar e criar trilhas de ensino por parte dos professores e execução das aulas para os alunos.',
+    imagePath: '/viamaker_logo.jpg',
+    role: 'Desenvolvedor React Front End',
+    time: 'Jan, 2021 - Mai, 2021',
+  },
+  {
+    enterpriseName: 'UKOR',
+    description:
+      'Desenvolvi projetos para melhorar a experiência do usuário e otimizar ferramentas da empresa, como um aplicativo de saúde e bem-estar em React Native, Redux e Node.js, um portal administrativo em ReactJS, Node.js e Redux para gestão de operações e dados, e landing pages otimizadas para divulgação e engajamento do público.',
+    imagePath: '/ukor_logo.jpg',
+    role: 'Full Stack Developer',
+    time: 'Abr, 2019 - Jan, 2021',
+  },
+  {
+    enterpriseName: 'CIEE - Centro de Integração Empresa-Escola',
+    description:
+      'Responsável por auxiliar e fornecer suporte a professores para criação e aplicações de aulas pré programadas para os alunos, também responsável por cuidar do laboratório em geral e dos demais computadores do local.',
+    imagePath: '/ciee_logo.jpg',
+    role: 'Laboratorista',
+    time: 'Fev, 2018 - Abr, 2019',
+    isTheFirst: true,
+  },
+];
