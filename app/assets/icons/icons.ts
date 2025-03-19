e07582018bc54d8efc3f2727f8fab60c44c003d4
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-19 15:14:21 -0300

    Adicionando arquivo para exportacao de icones

---
+++ app/assets/icons/icons.ts
@@ -0,0 +1,45 @@
+import react from './react.svg';
+import typescript from './typescript.svg';
+import nextjs from './nextjs.svg';
+import redux from './redux.svg';
+import styledcomponents from './styledcomponents.svg';
+import storybook from './storybook.svg';
+import reactquery from './reactquery.svg';
+import mysql from './mysql.svg';
+import git from './git.svg';
+import postgresql from './postgresql.svg';
+import nodejs from './nodedotjs.svg';
+import express from './express.svg';
+import nestjs from './nestjs.svg';
+import sequelize from './sequelize.svg';
+import prisma from './prisma.svg';
+import firebase from './firebase.svg';
+import docker from './docker.svg';
+import github from './github.svg';
+import jest from './jest.svg';
+import swagger from './swagger.svg';
+
+const icons = {
+  react,
+  typescript,
+  nextjs,
+  redux,
+  styledcomponents,
+  storybook,
+  reactquery,
+  mysql,
+  git,
+  postgresql,
+  nodejs,
+  express,
+  nestjs,
+  sequelize,
+  prisma,
+  firebase,
+  docker,
+  github,
+  jest,
+  swagger,
+};
+
+export default icons;
