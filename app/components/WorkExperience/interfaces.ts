a01f375f4d76bde6f4475ebcad5e3582ea4f0146
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-03-31 16:59:38 -0300

    Atualizando interface de workExperience

--- app/components/WorkExperience/interfaces.ts
+++ app/components/WorkExperience/interfaces.ts
@@ -1,8 +1,16 @@
+interface ITechs {
+  name: string;
+  icon: string;
+}
+
 export interface IWorkExperience {
   enterpriseName: string;
   role: string;
   time: string;
   description: string;
+  detailedDescription: string;
   imagePath: string;
   isTheFirst?: boolean;
+  techs?: ITechs[];
+  link: string;
 }
