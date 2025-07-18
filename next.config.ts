f21f245a2e9adb1485be410aa9183fc56d4a3dc2
Joao Vitor Otavio <joaovitor2201@hotmail.com>
2025-07-18 13:01:25 -0300

    Corrigindo warning de mismatch

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
